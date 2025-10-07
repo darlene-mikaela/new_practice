a2d88e6145ec7fe69c882396ca54940c431cc369
darlenemikaela <darlenemikaela.id@gmail.com>
2025-10-07 15:22:06 +0800

    add flexibility for entering number

--- multiples.cpp
+++ multiples.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 
 int main() {
-    for (int i = 1; i <= 10; i++) {
-        for (int j = 1; j <= 10; j++) {
+    int max = 9;
+    for (int i = 1; i <= max; i++) {
+        for (int j = 1; j <= max; j++) {
             cout << i << "*" << j << "=" << i*j << "\t";
         }
         cout << endl;
