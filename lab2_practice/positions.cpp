08d2fa09ee1a473776da39d8ca737b0312025ccc
darlenemikaela <darlenemikaela.id@gmail.com>
2025-10-11 15:32:47 +0800

    corrected positions.cpp

--- lab2_practice/positions.cpp
+++ lab2_practice/positions.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 int main() {
-    int pos = 0; // initialize position to 0
     int k, x1, x2, y1, y2;
     cin >> k;  // 生命值
-    cin >> x1 >> x2;
-    cin >> y1 >> y2;
+    cin >> x1 >> y1;
+    cin >> x2 >> y2;
+    int pos = 0; // initialize position to 0
     
     while (k > 0) {
-        pos = pos+k;
+        pos += k;
         if (pos%x1 == 0) {
             k -= y1;
         }
