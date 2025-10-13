b76b6a0ea9c16305d4080ed32f61ce05eaa63c75
darlenemikaela <darlenemikaela.id@gmail.com>
2025-10-13 19:19:54 +0800

    edited alarmClock.cpp

--- lab2_practice/alarmClock.cpp
+++ lab2_practice/alarmClock.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main() {
-    int h1,m1,h2,m2;
-    int time;
+    int h1,m1,h2,m2,time;
 
-    while (cin >> h1 >> m1 >> h2 >> m2) {
-        if (h1==0 && h2==0 && m1==0 && m2==0) { break; }
+    while (h1!=0 || m1!=0 || h2!=0 || m2!=0) {
+        cin >> h1 >> m1 >> h2 >> m2;
+        if (h1==0 && m1==0 && h2==0 && m2==0) break;
         time = (h2-h1)*60 + (m2-m1);
         time = (time<0?time+1440:time); // +24 hours
         cout << time << endl;
